1766319995f91814c9ee953f005ce0193ca201d2
Cristian Quintana <cquintan@cern.ch>
2026-04-22 10:06:44 +0200

    Change binnign runtime working

--- CameraServer/test/testCameraServer.py
+++ CameraServer/test/testCameraServer.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     camera = Camera('picture.png')
     camera.start_acquisition().set_exposure(1/250)
 
-    camera.change_binning_runtime(2,2)
+    #camera.change_binning_runtime(2,2)
 
     server = CameraServer(options.ip, options.port, camera)
     
