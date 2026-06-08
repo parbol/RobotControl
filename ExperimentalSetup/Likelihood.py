1f7a4a5168a90ba2450b002f9cf9bef483162144
parbol <pablom@cern.ch>
2026-06-08 09:22:24 +0200

    State of the art of calibration

--- ExperimentalSetup/Likelihood.py
+++ ExperimentalSetup/Likelihood.py
@@ -29,13 +29,10 @@ class CameraLikelihood(GenericLikelihoodModel):
         # Update camera parameters on each iteration X,Y
         self.robot.camera.r0[0] = params[0]
         self.robot.camera.r0[1] = params[1]
-        #self.robot.camera.cy = params[2]
-        #self.robot.camera.cx = params[2]
         self.robot.camera.psi = params[2]
         self.robot.camera.update()
-        # Compute Log-Like value
+        
         chi2 = 0.0
-
         for i in range(self.n):
 
             #Position of the robot
@@ -43,13 +40,10 @@ class CameraLikelihood(GenericLikelihoodModel):
             self.robot.JMoveRobotTo(robotPosition)
             #Nominal position of the point in global coordinates
             nominalPosition = self.endog[i]
-            #Nominal position of the point in the camera projection
-            xn, yn = self.robot.point3DToCameraProjection(nominalPosition)
-            rn = np.asarray([xn, yn])
-            #Measurement in the camera system 
-            rm = np.asarray([self.exog[i][4], self.exog[i][5]])
-            chi2 += np.linalg.norm(rn-rm)**2
-        self.chi2k.append(chi2)
+            pm = np.asarray([self.exog[i][4], self.exog[i][5]])
+            rm = self.robot.cameraProjectionToPoint3D(pm)
+            chi2 = chi2 + (nominalPosition[0]-rm[0])**2+(nominalPosition[1]-rm[1])**2 
+        self.chi2k.append(np.sqrt(chi2/self.n))
         return -chi2
 
 
@@ -65,27 +59,20 @@ class CameraLikelihood(GenericLikelihoodModel):
             self.robot.JMoveRobotTo(robotPosition)
             #Nominal position of the point in global coordinates
             nominalPosition = self.endog[i]
-            #Nominal position of the point in the camera projection
-            xn, yn = self.robot.point3DToCameraProjection(nominalPosition)
-            rn = np.asarray([xn, yn])
-            #Measurement in the camera system 
-            rm = np.asarray([self.exog[i][4], self.exog[i][5]])
-            chi2 += np.linalg.norm(rn-rm)**2
-            
+            pm = np.asarray([self.exog[i][4], self.exog[i][5]])
+            rm = self.robot.cameraProjectionToPoint3D(pm)
+            chi2 = chi2 + (nominalPosition[0]-rm[0])**2+(nominalPosition[1]-rm[1])**2
             print('------------------------------------------')
             print('Nominal nominalPosition', nominalPosition)
-            print('(xn, yn) =', xn, yn)
-            print('(xm, ym) =', self.exog[i][4], self.exog[i][5])
+            print('Estimated position =', rm)
             print('Robot position: j1, j2, j3, j4', self.exog[i][0], self.exog[i][1], self.exog[i][2], self.exog[i][3])
             print('Robot position:', self.robot.position)
-            print('Parameters:', self.robot.camera.r0[0], self.robot.camera.r0[1], self.robot.camera.cx, self.robot.camera.cy)
+            print('Parameters:', self.robot.camera.r0[0], self.robot.camera.r0[1], self.robot.camera.psi)
             print('------------------------------------------')
-            
-        
         return chi2
 
 
-    def fit(self, start_params=None, method='bfgs', maxiter=10000, **kwargs):
+    def fit(self, start_params=None, method='bfgs', maxiter=100000, **kwargs):
         # methods = bfgs, lbfgs, nm, newton, powell, cg, ncg, basinhopping, minimize
         # Only 'powell' method converges and get low chi**2 -> Use POWELL
 
@@ -93,8 +80,9 @@ class CameraLikelihood(GenericLikelihoodModel):
             # Set initial values for the parameters to optimize
             #start_params = [self.robot.camera.r0[0], self.robot.camera.r0[1], self.robot.camera.cx]
             #start_params = [self.robot.camera.r0[0], self.robot.camera.r0[1], self.robot.camera.cx, -0.5]
-            start_params = [self.robot.camera.r0[0], self.robot.camera.r0[1], 1.5]
-       
+            start_params = [self.robot.camera.r0[0], self.robot.camera.r0[1], self.robot.camera.psi]
+            #start_params = [self.robot.camera.psi]
+ 
         # Call the parent class's fit method
         return super(CameraLikelihood, self).fit(start_params=start_params, method=method, maxiter=maxiter, **kwargs)
     
